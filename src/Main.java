40e251c7ba71ed6f579f0dcefb85a75f4204a608
Yasin <121798700+yasinztrk@users.noreply.github.com>
2023-02-18 11:58:24 +0300

    sayiBulma

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] sayilar = new int[]{1, 2, 5, 7, 0};
+        int aranacak = 8;
+        boolean varMı = false;
+        for (int sayi : sayilar) {
+            if (aranacak == sayi) {
+                varMı = true;
+                break;
+
+            }
+        }
+        if (varMı) {
+            System.out.println("Sayı mevcuttur");
+        } else {
+            System.out.println("Sayı mevcut değildir.");
+        }
+
+
+    }
+}
